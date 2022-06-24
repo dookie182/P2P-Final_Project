6dbadc3ed4d62478eabafe4f6b776a8a901314f1
dookie182 <franckot@gmail.com>
2022-06-24 17:57:35 +0200

    First Commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,226 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./LotteryMint.sol";
+
+contract Lottery{
+
+    //Data struct to manage player's address and played numbers
+    struct Ticket{
+        address playerAddress;
+        uint [] playedNumbers;
+    }
+
+    uint private constant TICKET_PRICE = 1 ether; 
+    address payable private lotteryOperator; //Address of the lottery operator
+    
+    uint private ticketingCloses;
+
+    //Declaration of LotteryMint contract
+    LotteryMint item;
+    
+    //List of NFTs IDs
+    uint256 [8] private NFTList;
+    
+    //Array for sorted winning numbers
+    uint256 [6] private winningNumbers; 
+    
+    uint8 private previousValue;
+    Ticket [] private tickets;
+    uint private lotteryDuration;
+    uint private DEBUG = 0;
+    uint private K = 2;
+
+    // Events 
+    event ticketBuy(address player);
+    event roundClosed();
+    event roundStart();
+    event NFTMinted(address NFTOwner, uint256 newNFTId);
+    event awardPlayer(address player, uint256 prize);
+    event numbersDrawn();
+    event lotteryClosed();
+
+    
+    constructor (uint duration){
+        lotteryOperator = payable(msg.sender);
+        item = new LotteryMint();
+        ticketingCloses = block.number + duration;
+        lotteryDuration = duration;
+        
+        for(uint i = 1; i < 9; i++){
+            mint(i);
+        }
+        
+    }
+    
+    //Function to buy a new Ticket and to choose numbers to play
+    function buy (uint [] memory numbers) public payable { 
+        require(block.number < ticketingCloses, "Lottery round already closed, wait for a new round please!");
+        require(msg.value == TICKET_PRICE, "Not enough money, Ticket Price --> 1 Ether"); 
+        require(numbers.length == 6, "Please choose exactly six different numbers!");
+        Ticket memory newTicket;
+        newTicket.playerAddress = msg.sender;
+        newTicket.playedNumbers = numbers;
+        tickets.push(newTicket);
+        emit ticketBuy(msg.sender);
+    }
+
+    //Function to select and award winners
+    function givePrizes () private {
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        require(block.number > ticketingCloses, "Round still open, please wait lottery round ends!"); 
+
+        uint guessed = 0;
+        uint class = 0;
+        bool powerBall = false;
+        
+        for(uint i = 0; i < tickets.length ;i++){
+            Ticket memory player = tickets[i];
+            for(uint j = 0; j < player.playedNumbers.length - 1 ; j++){
+                uint num = player.playedNumbers[j];
+                for(uint k = 0; k < 5; k++){
+                    if(num == winningNumbers[k]){
+                        guessed++;
+                    }
+                }
+            }
+            if(player.playedNumbers[5] == winningNumbers[5]){
+                powerBall = true;
+            }
+            class = uint(selectClass(guessed,powerBall));
+
+            if(class > 0){
+                item.sendNFT(player.playerAddress, NFTList[class - 1]);
+                emit awardPlayer(player.playerAddress, NFTList[class - 1]);
+                mint(class);
+            }
+            powerBall = false;
+            guessed = 0;
+        }
+    }
+
+    //Function used to select correct prize class
+    function selectClass (uint guessed, bool powerBall) private pure returns(uint) {
+        uint prize = 0;
+        
+        if(guessed == 0 && powerBall){
+            prize = 8;
+        }else{
+            if(guessed == 1 && !powerBall){
+                prize = 7;
+            }else{
+                if(guessed == 2 && !powerBall || (guessed == 1 && powerBall)){
+                    prize = 6;
+                }else{
+                    if(guessed == 3 && !powerBall || (guessed == 2 && powerBall)){
+                        prize = 5;
+                    }else{
+                        if(guessed == 4 && !powerBall || (guessed == 3 && powerBall)){
+                            prize = 4;
+                        }else{
+                            if(guessed == 4 && powerBall){
+                                prize = 3;
+                            }else{
+                                if(guessed == 5 && !powerBall){
+                                    prize = 2;
+                                }else{
+                                    if(guessed == 5 && powerBall){
+                                        prize = 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return prize;
+    }
+    //Function used to start a new lottery round
+    function startNewRound () public {
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        require(block.number > ticketingCloses, "Round still open, please wait lottery round ends!");
+        delete winningNumbers;
+        delete tickets;
+        ticketingCloses = block.number + lotteryDuration;
+        emit roundStart();
+    }
+
+    //Function for lottery operator to draw winning numbers
+    function drawNumbers () private {
+        if(DEBUG == 1){
+        for (uint i = 0; i < 5; i++){
+            winningNumbers[i] = i+1;
+        }
+        winningNumbers[5] = 6;
+        }else{
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        require(block.number > ticketingCloses, "Round still open, please wait lottery round ends!"); 
+        for (uint i = 0; i < 6; i++){
+            
+            if(i == 5){
+                winningNumbers[i] = rand(26);
+            }
+            else{
+                winningNumbers[i] = rand(69);
+            }
+            for(uint j = 0; j <= i; j++){
+                if(winningNumbers[i] == winningNumbers[j] && i != 5){
+                    winningNumbers[i] = rand(69);
+                }
+                if(winningNumbers[i] == winningNumbers[j] && i == 5){
+                    winningNumbers[i] = rand(26);
+                }
+            }
+        }
+        }
+
+        emit numbersDrawn();
+    }
+
+    //Function used draw numbers and give prizes. Only the lottery operator can call this function.
+    function closeRound () public{
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        require(block.number > ticketingCloses, "Round still open, please wait lottery round ends!"); 
+        drawNumbers();
+        givePrizes();
+        withdraw();
+        emit roundClosed();
+    }
+
+    //Function used to close the Lottery and deactivate  the contract
+    function closeLottery () public {
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        address payable refunded;
+        if (block.number <= ticketingCloses){
+            for (uint i = 0; i < tickets.length ;i++){
+                refunded = payable(tickets[i].playerAddress);
+                refunded.transfer(TICKET_PRICE);
+            }
+        }
+        emit lotteryClosed();
+        selfdestruct(payable(msg.sender));
+    }
+
+    //Function used to mint a new NFT
+    function mint (uint class) private{
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        uint256 NFT_ID = uint256(item.awardItem(class));
+        NFTList[class - 1] = NFT_ID;
+        emit NFTMinted(msg.sender, NFT_ID);
+    }
+    
+    //Function used by the lottery operator to send contract balance to  the lottery operator's address
+    function withdraw () private { 
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        require(block.number > ticketingCloses, "Round still open, please wait lottery round ends!"); 
+        lotteryOperator.transfer(address(this).balance);
+     }
+
+    //Function used to deterministically extract pseudo-random winning numbers for the lottery
+    function rand(uint modulus) private returns (uint8) {
+        previousValue = uint8((uint256(keccak256(abi.encodePacked(block.number + K, previousValue))))% modulus);
+        return previousValue;
+    }
+}
+
---
+++ contracts/LotteryMint.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract LotteryMint is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    
+    string [8] public URI;
+    uint256 [] public tokens;
+    address lotteryOperator;
+
+
+
+    constructor() ERC721("LotteryMint", "LTM") {
+        
+        lotteryOperator = msg.sender;
+        URI[0] = "https://ipfs.io/ipfs/QmVR8saozo2wniS3VxA4jAdiJAmEe2r2Hg6F9csiQrd3os?filename=Petra%20-%20Giordania.jpg";
+        URI[1] = "https://ipfs.io/ipfs/QmeAa7KYRexb4q8P9vVPqHyR4xfw5Db6GmeXKpfGzCZuJz?filename=Grande%20Muraglia%20Cinese%20-%20Cina.jpg";
+        URI[2] = "https://ipfs.io/ipfs/QmV7M4my4uRwgLNtjBN4PFC2WjUceahiKfbXNtREsvWLta?filename=Colosseo%20-%20Italia.jpg";
+        URI[3] = "https://ipfs.io/ipfs/Qmb4TXhHqtwv2XALGfY9iznbb5bp8dYpiZcWF8BhiAaNYg?filename=Machu%20Picchu%20-%20Peru%CC%80.jpg";
+        URI[4] = "https://ipfs.io/ipfs/QmSJXaUeyeQFsArZbFvg2adXbyMbVdz24644gowt92DBHg?filename=Cristo%20Redentore%20-%20Brasile.jpg";
+        URI[5] = "https://ipfs.io/ipfs/QmSniYADMf7NDm4SXba37q5moPcHQfsGkrKVavwz84JhX4?filename=Chiche%CC%81n%20Itza%CC%81%20-%20Messico.jpg";
+        URI[6] = "https://ipfs.io/ipfs/QmerWLUCimRVAveEBPW2MQmLZ5VDq24W8y9vQ2wNFnd9jj?filename=Taj%20Mahal%20-%20India.jpg";
+        URI[7] = "https://ipfs.io/ipfs/QmXdZpwuPc3WgnCCR7NGLkvERX5sxNVdXQaVTEEZgVBATx?filename=7%20Meraviglie%20del%20mondo%20moderno.jpg";
+
+    }
+
+    function awardItem(uint class)
+        public
+        returns (uint256)
+    {
+        require(class > 0 && class < 9, "Please insert a correct class for the prize!");
+        uint256 newItemId = _tokenIds.current();
+        _mint(lotteryOperator, newItemId);
+        _setTokenURI(newItemId, URI[class - 1]);
+        _tokenIds.increment();
+        return newItemId;
+    }
+
+    function sendNFT (address player, uint256 tokenId) public{
+        require(msg.sender == lotteryOperator, "You are not the lottery operator, permission denied!");
+        safeTransferFrom(lotteryOperator, player, tokenId);
+    }
+
+
+}
